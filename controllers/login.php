144cca032b8e3ef4bceb99c00a7aa73bad96b255
NellyRJ <nelly.rangelj@uanl.edu.mx>
2023-10-15 23:16:03 -0600

    login update

--- controllers/login.php
+++ controllers/login.php
@@ -5,11 +5,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     //Obtener Json
     $json = json_decode(file_get_contents('php://input'),true);
-    //print_r(json_encode($json));
-    $conexion = new Conexion;
-    $mysqli = $conexion->conexion;
-    $idUsr = Usuario::validateCredendtials($mysqli, $json['userName'], $json['contrasena']);
-    //print_r($idUsr);
+    print_r($json);
     
     //Sanitizar JSON
     // $filters = [
@@ -24,28 +20,25 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     // foreach($json as $key=>$value) {
     //     $json_safe[$key] = filter_var($value, $filters[$key], $options[$key]);
     // }
-
     header('Content-Type: application/json');
+    $conexion = new Conexion;
+    $mysqli = $conexion->conexion;
+    $user = Usuario::findUserByUsername($mysqli,$json["username"],$json["password"]);
     $json_response = ["success" => true];
-
-    if($idUsr > 0) {
-        $user = Usuario::findUserById($mysqli, $idUsr);
-        //print_r(($user));
-        $json_response["msg"]= "Bienvenido";
+    if($user) {
+        $json_response["msg" ]= "Bienvenido";
         $json_response ["user"] = $user->toJSON();
         //Iniicamos la sesion
         session_start();
         //Guardamos el ID del usuario en la sesion
-        $_SESSION["AUTH"] = (string)$user->getIdUsuario();
+        $_SESSION["AUTH"] = (string)$user->getId();
         echo json_encode($json_response);
         exit;
-
     } else {
         $json_response["success"]  = false;
         $json_response["msg"] = "El usuario o la contraseña no son correctos";
         echo json_encode($json_response);
         exit;
-    }
-    
+    } 
    
 }
