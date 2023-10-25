4228293913b34ebee5058f89dcd7c74d85a690e7
Enoch Galindo <59318923+Enoch454@users.noreply.github.com>
2023-10-25 18:46:11 +0000

    Primera prueba con index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 
-require_once "./views/index.php";
+require_once __DIR__.'/router.php';
+
+require_once './controllers/home.php';
+
+// ##################################################
+// ##################################################
+// ##################################################
+
+// Static GET
+// In the URL -> http://localhost
+// The output -> Index
+get('/', Home::verIndex());
+
+// For GET or POST
+// The 404.php which is inside the views folder will be called
+// The 404.php has access to $_GET and $_POST
+//any('/404','views/404.php');
